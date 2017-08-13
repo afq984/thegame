2498f99cf316f34e4ba63dd1f0a57f47c6b4a67b
afg <afg984@gmail.com>
2017-08-14 05:24:30 +0800

    client: update example

--- client/python/example.py
+++ client/python/example.py
@@ -4,12 +4,16 @@ from thegame import Client, Ability
 class IClient(Client):
     def action(self, hero, polygons, heroes, bullets):
         print("I'm", hero)
+        print(
+            f'level: {hero.level}',
+            f'experience: {hero.experience}/{hero.experience_to_level_up}'
+        )
         print("I'm surrounded by these polygons:", polygons)
         print("I'm surrounded by these heroes:", heroes)
         print("I'm surrounded by these bullets:", bullets)
         print('-' * 79)
         if polygons:
-            self.shoot(*polygons[0].position)
+            self.shoot_at(*polygons[0].position)
 
 
 IClient.main()
