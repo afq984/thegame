dc50d679c522fdb889fc78e0c25a597aad11cff5
afg <afg984@gmail.com>
2017-08-16 12:47:05 +0800

    buff body damage

--- server/go/src/thegame/ability.go
+++ server/go/src/thegame/ability.go
@@ -15,7 +15,7 @@ const (
 var AbilityValues = [NAbilities][9]int{
 	{0, 1, 2, 3, 4, 5, 6, 7, 8},                            // HealthRegen
 	{1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000}, // MaxHealth
-	{16, 20, 24, 28, 32, 36, 40, 44, 48},                   // BodyDamage
+	{20, 25, 30, 35, 40, 45, 50, 55, 60},                   // BodyDamage
 	{8, 9, 10, 11, 12, 13, 14, 15, 16},                     // BulletSpeed
 	{40, 50, 60, 70, 80, 90, 100, 110, 120},                // BulletPenetration
 	{12, 15, 18, 21, 24, 27, 30, 33, 36},                   // BulletDamage
