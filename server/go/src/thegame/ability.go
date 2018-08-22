a6848ca2201d7e02d70bd02678fa1eb5ee0c9dc1
afg <afg984@gmail.com>
2018-08-23 05:51:25 +0800

    server/go: ability: add linearScale function

--- server/go/src/thegame/ability.go
+++ server/go/src/thegame/ability.go
@@ -12,13 +12,21 @@ const (
 	NAbilities
 )
 
+func linearScale(at0, at8 int) [9]int {
+	step8 := at8 - at0
+	L := func(i int) int {
+		return at0 + int(float64((step8)*i)/8)
+	}
+	return [9]int{L(0), L(1), L(2), L(3), L(4), L(5), L(6), L(7), L(8)}
+}
+
 var AbilityValues = [NAbilities][9]int{
-	{0, 1, 2, 3, 4, 5, 6, 7, 8},                            // HealthRegen
-	{1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000}, // MaxHealth
-	{20, 25, 30, 35, 40, 45, 50, 55, 60},                   // BodyDamage
-	{8, 9, 10, 11, 12, 13, 14, 15, 16},                     // BulletSpeed
-	{40, 50, 60, 70, 80, 90, 100, 110, 120},                // BulletPenetration
-	{12, 15, 18, 21, 24, 27, 30, 33, 36},                   // BulletDamage
-	{15, 13, 11, 9, 7, 6, 5, 4, 3},                         // Reload
-	{6, 7, 8, 9, 10, 11, 12, 13, 14},                       // MovementSpeed
+	linearScale(0, 8),              // HealthRegen
+	linearScale(1000, 5000),        // MaxHealth
+	linearScale(20, 60),            // BodyDamage
+	linearScale(8, 16),             // BulletSpeed
+	linearScale(40, 120),           // BulletPenetration
+	linearScale(12, 36),            // BulletDamage
+	{15, 13, 11, 9, 7, 6, 5, 4, 3}, // Reload
+	linearScale(6, 14),             // MovementSpeed
 }
